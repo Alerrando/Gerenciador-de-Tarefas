e0b4713829a1c8aad788c4732215029d177049f0
Alerrando Breno <alerrando2@gmail.com>
2022-12-03 17:13:52 -0300

    using local storage

--- src/app/tarefa/tarefa.component.ts
+++ src/app/tarefa/tarefa.component.ts
@@ -11,22 +11,39 @@ export class TarefaComponent {
   
   ngOnInit(): void{
     const tarefas = localStorage.getItem(this.TAREFA_KEY);
+    if(tarefas){
+      this.listaTarefas = JSON.parse(tarefas);
+    }
   }
 
   adicionar(nomeTarefa:string){
     if(nomeTarefa.trim().length == 0){ return; }
     const tarefaEncontrada = this.listaTarefas.find(item => item.nome.toLowerCase() == nomeTarefa.toLowerCase())
     
-    !tarefaEncontrada ? this.listaTarefas.push({id: this.listaTarefas.length, nome:nomeTarefa, concluida: false}) : alert(`Você já cadastrou a tarefa ${nomeTarefa}`)    
+    if(!tarefaEncontrada){ 
+      this.listaTarefas.push({id: this.listaTarefas.length, nome:nomeTarefa, concluida: false})
+      this.salvarLista()
+    } 
+    else
+      alert(`Você já cadastrou a tarefa ${nomeTarefa}`)    
   }
 
   deletar(id:number){
     this.listaTarefas = this.listaTarefas.filter(tarefa => tarefa.id !== id)
+    this.salvarLista()
   }
 
   completar(id:number){
     const tarefaEncontrada = this.listaTarefas.find(item => item.id == id);
 
-    if(TarefaComponent) {tarefaEncontrada.concluida = !tarefaEncontrada.concluida;}
+    if(TarefaComponent) {
+      tarefaEncontrada.concluida = !tarefaEncontrada.concluida;
+      this.salvarLista()
+    }
+    
+  }
+
+  private salvarLista(){
+    localStorage.setItem(this.TAREFA_KEY, JSON.stringify(this.listaTarefas));
   }
 }
